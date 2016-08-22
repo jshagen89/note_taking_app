0c4489ec6ef541aadf383d853bfe3c973aa1f645
jshagen <jshagen@tlu.edu>
2016-08-22 13:37:31 -0500

    Formatted Created Date form time stamp in db. Added additional text view field for date to populate in main activity.

--- app/src/main/java/com/example/joseph/takenote/NotesCursorAdapter.java
+++ app/src/main/java/com/example/joseph/takenote/NotesCursorAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 public class NotesCursorAdapter extends CursorAdapter{
 
     public NotesCursorAdapter(Context context, Cursor c, int flags) {
+
         super(context, c, flags);
     }
 
@@ -19,10 +20,23 @@ public class NotesCursorAdapter extends CursorAdapter{
         return LayoutInflater.from(context).inflate(R.layout.note_list_item,parent,false);
     }
 
+    private String formatDateCreated(String date)
+    {
+        String[] timeStampParts = date.split(" ");
+        String[] dateParts = timeStampParts[0].split("-");
+        String[] timeParts = timeStampParts[1].split(":");
+
+        // Need to convert from military time to regular time here
+        
+        return (dateParts[1] + "-" + dateParts[2] + "-" + dateParts[0] + "  " + timeParts[0] + ":" + timeParts[1]);
+    }
+
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         // get text of note from database
         String noteText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_TEXT));
+        String created_date = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_CREATED));
+        String formattedDate = formatDateCreated(created_date);
 
         // determine if the note text contains a line feed character
         int position = noteText.indexOf(10);
@@ -32,7 +46,11 @@ public class NotesCursorAdapter extends CursorAdapter{
         }
 
         // Set the text of the view with the note text
-        TextView myTV = (TextView) view.findViewById(R.id.tvNote);
-        myTV.setText(noteText);
+        TextView noteTV = (TextView) view.findViewById(R.id.tvNote);
+        noteTV.setText(noteText);
+
+        // Set the text of the date created field
+        TextView dateTV = (TextView) view.findViewById(R.id.date_created);
+        dateTV.setText(formattedDate);
     }
 }
